d9ad3b493a1fdf57d8a483309178183c7385cfc2
Vinícius Manoel <59481808+vncsmnl@users.noreply.github.com>
2024-01-31 14:24:46 -0300

    :zap: Upd: added delay param

--- main.js
+++ main.js
@@ -4,9 +4,10 @@
  * @param scriptText - The `scriptText` parameter is a string that represents the script you want to
  * send as a message in a conversation. Each line of the script should be separated by a newline
  * character (`\n`).
+ * @param delay - The `delay` parameter is the time delay (in milliseconds) between sending each message.
  * @returns the number of lines in the script that were successfully executed.
  */
-async function enviarScript(scriptText) {
+async function enviarScript(scriptText, delay = 1000) {
     const lines = scriptText.split('\n').map(line => line.trim()).filter(line => line);
 
     const main = document.querySelector("#main");
@@ -28,20 +29,19 @@ async function enviarScript(scriptText) {
         sendButton.click();
 
         if (lines.indexOf(line) !== lines.length - 1) {
-            await new Promise(resolve => setTimeout(resolve, 250));
+            await new Promise(resolve => setTimeout(resolve, delay));
         }
     }
 
     return lines.length;
 }
 
-/* The code is defining a script as a multi-line string using backticks (`). The script contains text
-that will be sent as messages in a conversation. */
+// Example script with a delay of 2000 milliseconds (2 seconds) between messages
 const script = `
 Coloque o seu texto aqui
 Put your text here
 `;
 
-enviarScript(script)
+enviarScript(script, 2000)
     .then(e => console.log(`Código finalizado, ${e} mensagens enviadas`))
     .catch(console.error);
